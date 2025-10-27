02d830e70b22c20c0e408037b8ca22ba9bdd60b8
Auxciliya A <auxciliya1715@gmail.com>
2025-10-27 17:41:52 +0530

    Added /api/send-summary route for cron trigger

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const todoRoutes = require('./routes/todo');
 const users = require('./routes/user');
 const auth = require('./routes/auth');
-//require('./mail/dailyEmail')
+const sendDailySummary = require('./mail/dailyEmail')
 
 const app = express();
 
@@ -18,6 +18,16 @@ app.use('/api/auth', auth);
 mongoose.connect(process.env.MONGO_URI)
 .then(() =>console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.get('/api/send-summary', async (req, res) => {
+  try {
+    await sendDailySummary();
+    res.send('✅ Daily summary emails sent successfully!');
+  } catch (err) {
+    console.error('Error sending daily summary:', err);
+    res.status(500).send('❌ Error sending daily summary: ' + err.message);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Connecting on port ${PORT}`));
