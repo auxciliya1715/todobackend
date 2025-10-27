68851be2601742fdf67226cd6a7c57443d8214a1
Auxciliya A <auxciliya1715@gmail.com>
2025-10-27 15:51:16 +0530

    Added Brevo email and daily summary cron job

--- mail/sendEmail.js
+++ mail/sendEmail.js
@@ -1,34 +1,44 @@
-const nodemailer = require('nodemailer');
-const generateEmailTemplate = require('./emailtemplate.js');
+//const nodemailer = require('nodemailer');
+const Brevo = require('@getbrevo/brevo');
+const generateEmailTemplate = require('./emailtemplate.js'); 
 
 async function sendEmail(userEmail, userName, todos, total, completed, pending) {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false
-      }
-    });
+ try{
+   const client = new Brevo.TransactionalEmailsApi();
+    client.authentications['apiKey'].apiKey = process.env.BREVO_API_KEY;
+  //  const transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     user: process.env.EMAIL_USER,
+  //     pass: process.env.EMAIL_PASS,
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false
+  //   }
+  
 
-    const quote = pending === 0
-      ? "Amazing work! You have completed everything today 🎉"
-      : "Keep pushing! Tomorrow is another chance to conquer your goals 💪";
+  const quote = pending === 0
+    ? "Amazing work! You have completed everything today 🎉"
+    : "Keep pushing! Tomorrow is another chance to conquer your goals 💪";
 
-    const html = generateEmailTemplate(userName, todos, total, completed, pending, quote);
+  const html = generateEmailTemplate(userName, todos, total, completed, pending, quote);
+  const sendSmtpEmail = new Brevo.SendSmtpEmail();
+    sendSmtpEmail.subject = "📊 Your Daily To-Do Summary";
+    sendSmtpEmail.htmlContent = html;
 
-    const mailOptions = {
-      from: `"To-Do Tracker" <${process.env.EMAIL_USER}>`,
-      to: userEmail,
-      subject: "📊 Your Daily To-Do Summary",
-      html,
-    };
+    sendSmtpEmail.sender = { name: "To-Do Tracker", email: "auxciliya1715@gmail.com" };
+    sendSmtpEmail.to = [{ email: userEmail, name: userName || "User" }];
 
-    await transporter.sendMail(mailOptions);
-    console.log(`Email sent successfully to ${userEmail}`);
+  // const mailOptions = {
+  //   from: `"To-Do Tracker" <${process.env.EMAIL_USER}>`,
+  //   to: userEmail,
+  //   subject: "📊 Your Daily To-Do Summary",
+  //   html,
+  // };
+   await client.sendTransacEmail(sendSmtpEmail);
+    console.log(`✅ Email sent successfully to ${userEmail}`);
+  //  await transporter.sendMail(mailOptions);
+  //   console.log(`Email sent successfully to ${userEmail}`);
   } catch (error) {
     console.error(`Error sending email to ${userEmail}:`, error);
   }
