9699b62c7ab437add6701f380578f37915b2d402
Auxciliya A <auxciliya1715@gmail.com>
2025-10-27 17:04:57 +0530

    Remove fixed port to fix Render deployment

--- index.js
+++ index.js
@@ -20,6 +20,4 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.error('Could not connect to MongoDB', err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Connecting on port ${PORT}`));
