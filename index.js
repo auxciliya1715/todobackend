d2561e9324a52bc51ac4e1978c3fe6e2d023a84e
Auxciliya A <auxciliya1715@gmail.com>
2025-11-04 13:52:49 +0530

    updated cors team

--- index.js
+++ index.js
@@ -11,11 +11,21 @@ const teamRoutes = require('./routes/teamRoutes');
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "x-auth-token"]
-}));
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:3000'];
+  const origin = req.headers.origin;
+
+  if (
+    allowedOrigins.includes(origin) ||
+    (origin && origin.endsWith('.netlify.app'))
+  ) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-auth-token');
+  next();
+});
 
 app.use(express.json());
 app.use('/api/todos', todoRoutes);
